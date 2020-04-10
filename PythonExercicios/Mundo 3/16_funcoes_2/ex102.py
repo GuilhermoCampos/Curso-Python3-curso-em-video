cc1f628768c1456e2d08390938792449236e3ff6
Guilherme Campos <GuilhermoCampos@users.noreply.github.com>
2020-04-10 12:45:53 -0300

    Create ex102.py

---
+++ PythonExercicios/Mundo 3/16_funcoes_2/ex102.py
@@ -0,0 +1,31 @@
+# Programa que tem uma função fatorial() que recebe dois Parâmetros: o primeiro que indica o número
+# a calcular e o outro chamado show, que será um valor lógico(opcional) indicando se será mostrado
+# ou não na tela o processo de cálculo do fatorial
+
+
+def fatorial(numero, show=False):
+    from time import sleep
+    print(f'!{numero} = ', end='')
+    sleep(1)
+    c = numero
+    f = 1
+    while c > 0:
+        if show:
+            sleep(0.5)
+            print(c, end='')
+            sleep(0.5)
+            print(' x ' if c > 1 else ' = ', end='')
+        f *= c
+        c -= 1
+    return f
+
+
+# Programa Principal
+print('Calculadora fatorial')
+num = int(input('Digite o número: !'))
+mostrar = str(input('Deseja mostrar a conta?[S/N]: ')).strip().upper()[0]
+if mostrar in 'S':
+    show = True
+else:
+    show = False
+print(fatorial(num, show))
