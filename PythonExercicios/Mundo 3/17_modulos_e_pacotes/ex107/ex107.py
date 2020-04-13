3a7bca217a21c22af675d50806e56fca3ec9f802
Guilherme Campos <GuilhermoCampos@users.noreply.github.com>
2020-04-13 14:34:14 -0300

    create ex107

---
+++ PythonExercicios/Mundo 3/17_modulos_e_pacotes/ex107/ex107.py
@@ -0,0 +1,12 @@
+# Crie um módulo chamado moeda.py que tenha as funções 
+# incorporadas aumentar(), diminuir(), doro() e metade.
+# Faça também um programa que importe esses módulo e
+# use algumas dessas funções
+import moeda
+
+num = float(input('Digite um valor: '))
+
+print(f'O dobro do valor {num} é {moeda.dobro(num)}')
+print(f'A metade do valor {num} é {moeda.metade(num)}')
+print(f'O valor {num} aumentado em 10% é {moeda.aumentar(num, 10)}')
+print(f'O valor {num} diminuido em 10% é {moeda.diminuir(num, 10)}')
