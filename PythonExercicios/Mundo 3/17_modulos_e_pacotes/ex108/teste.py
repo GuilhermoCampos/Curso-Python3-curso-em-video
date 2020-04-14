b489f7766552da7a6e616553c6c53118b0879416
Guilherme Campos <GuilhermoCampos@users.noreply.github.com>
2020-04-14 14:22:09 -0300

    Update teste.py

--- PythonExercicios/Mundo 3/17_modulos_e_pacotes/ex108/teste.py
+++ PythonExercicios/Mundo 3/17_modulos_e_pacotes/ex108/teste.py
@@ -10,4 +10,3 @@ print(f'O dobro do valor {moeda.moeda(num)} é {moeda.moeda(moeda.dobro(num))}')
 print(f'A metade do valor {moeda.moeda(num)} é {moeda.moeda(moeda.metade(num))}')
 print(f'O valor {moeda.moeda(num)} aumentado em 10% é {moeda.moeda(moeda.aumentar(num, 10))}')
 print(f'O valor {moeda.moeda(num)} diminuido em 10% é {moeda.moeda(moeda.diminuir(num, 10))}')
-3
