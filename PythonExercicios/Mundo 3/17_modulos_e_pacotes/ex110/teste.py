9a46cd41ce1bba660adec98fadded45ffd9bd407
Guilherme Campos <GuilhermoCampos@users.noreply.github.com>
2020-04-16 18:14:56 -0300

    Ex110

---
+++ PythonExercicios/Mundo 3/17_modulos_e_pacotes/ex110/teste.py
@@ -0,0 +1,9 @@
+# Adicione ao módulo moeda.py criado nos desafios anteriores, uma função chamada resumo(),
+# que mostre na tela algumas informações geradas pelas funções que já temos no módulo
+# criado até aqui
+
+import moeda2
+
+# Programa Principal
+num = float(input('Digite um valor: '))
+moeda2.resumo(num, 80, 35)
